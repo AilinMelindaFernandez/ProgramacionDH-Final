6089b554fff7590e954c9cc1c2a8013ed314270d
zoxhe <zadisanchez@udesa.edu.ar>
2022-06-10 11:28:13 -0300

    canciones

--- programacionDH/js/index.js
+++ programacionDH/js/index.js
@@ -32,9 +32,8 @@ fetch(url)
         console.log(error);
     })
 
-    let urla = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart";
-
-fetch(urla)
+    
+fetch(url)
     .then(function(response){
         return response.json();
     })
@@ -52,6 +51,40 @@ fetch(urla)
 
             contenido += `<p><img src=${info[i].artist.picture}><p>`, 
             contenido += `<p>nombre:${info[i].type}<p>`, 
+            contenido += `<p>tipo:${info[i].title}<p>`
+        }
+
+        console.log(contenido);
+
+        albumList.innerHTML += contenido;
+
+    })
+
+
+    .catch(function(error){
+        console.log(error);
+    })
+
+    
+
+fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+
+    .then(function(data){
+        console.log(data);
+
+        let info = data.tracks.data;
+
+        let albumList = document.querySelector(".contenedorcanciones");
+        let contenido = "";
+
+        for(let i=0; i<info.length; i++){
+            //construir un elemento de lista
+
+            contenido += `<p><img src=${info[i].artist.picture}><p>`, 
+            contenido += `<p>nombre:${info[i].artist}<p>`, 
             contenido += `<p>tipo:${info[i].title}<p>`
         }
 
