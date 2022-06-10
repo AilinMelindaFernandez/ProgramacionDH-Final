9f43e89cde271171334ec796d14a8f34281c85ad
AilinMelindaFernandez <afernandez@udesa.edu.ar>
2022-06-10 10:36:42 -0300

    albums y artistas api

--- programacionDH/js/index.js
+++ programacionDH/js/index.js
@@ -31,3 +31,40 @@ fetch(url)
     .catch(function(error){
         console.log(error);
     })
+
+
+
+    let urla = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart";
+
+fetch(urla)
+    .then(function(response){
+        return response.json();
+    })
+
+
+    .then(function(data){
+        console.log(data);
+
+        let info = data.albums.data;
+
+        let characterList = document.querySelector(".albums");
+        let contenido = "";
+
+        for(let i=0; i<info.length; i++){
+            //construir un elemento de lista
+
+            contenido += `<p><img src=${info[i].artist.picture}><p>`, 
+            contenido += `<p>nombre:${info[i].type}<p>`, 
+            contenido += `<p>tipo:${info[i].title}<p>`
+        }
+
+        console.log(contenido);
+
+        characterList.innerHTML += contenido;
+
+    })
+
+
+    .catch(function(error){
+        console.log(error);
+    })
