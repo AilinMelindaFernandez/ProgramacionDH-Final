b937ed3341b3e9d24e8466ccec6be9023b8476c3
AilinMelindaFernandez <afernandez@udesa.edu.ar>
2022-06-12 06:39:27 -0300

    detail-artist terminado. Caps que faltaria agregarle un poco mas de info, pero nose como sacarla de la apiporque no estan los parametros. En todos los html cambie los links de la barra de navegacion para que nos lleve a las parte de la pag(menos el del buscador). Tambien cambie algunos estilos del index porque no se veian bien o porque si los dejaba asi no funcionaban en el js.

--- programacionDH/js/detail-artist.js
+++ programacionDH/js/detail-artist.js
@@ -3,21 +3,55 @@ let qsToObject = new URLSearchParams(queryString); //Un objeto literal basado en
 let idArtista = qsToObject.get('id'); //obtengo el id
 
  
-let urlDetalle =`https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${idArtista}?`;
+let urlDetalleArtista =`https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${idArtista}?`;
 
-fetch(urlDetalle)
+fetch(urlDetalleArtista)
     .then(function(response){
         return response.json()
     })
     .then(function(data){
         console.log(data);
-        let title = document.querySelector('.contenedorinfoartist');
+        let name = document.querySelector('.titulosdetalleh3');
         let image = document.querySelector('img');
+        let cantidadDeFans = document.querySelector('.fans');
 
-        title.innerText = data.name;
-        image.src= data.picture
+       // cantidadDeFans.innerText = data.nb_fan;
+        cantidadDeFans.innerHTML = `<p>Fans ${data.nb_fan}</p>`
+        name.innerText = data.name;
+        image.src= data.picture_big;
 
     })
     .catch(function(error){
         console.log(error)
+    })
+
+    let urlDetalleArtistaAlbum =`https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${idArtista}/albums`;
+    
+fetch(urlDetalleArtistaAlbum)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        let info = data.data;
+
+        let albumList = document.querySelector('.olDeArtistaDetalle');
+        let contenido = "";
+
+        for(let i=0; i<5; i++){
+            //construir un elemento de lista
+
+            contenido += ` <li class="top5">
+                                <h4>${info[i].title}</h4>
+                            </li>` 
+        }
+
+        console.log(contenido);
+
+        albumList.innerHTML += contenido;
+
+    })
+    .catch(function(error){
+        console.log(error);
     })
